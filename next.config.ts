656abbffc3c7238ef3c8592ed5b70e2fd4cf4132
Rahul Das <rahulcodepython@gmail.com>
2025-03-08 14:21:53 +0530

    Remove unused HelloPage component and clean up configuration in Dockerfile and next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     /* config options here */
     output: "standalone",
-    env: {
-        API_URL: process.env.API_URL
-    }
 };
 
 export default nextConfig;
